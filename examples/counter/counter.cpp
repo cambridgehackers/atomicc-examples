3551a67b7170755b4a6cde87d27050a00179f546
Jamey Hicks <jamey.hicks@qrclab.com>
2014-08-15 12:47:58 -0400

    update counter so it compiles with g++

--- examples/counter/counter.cpp
+++ examples/counter/counter.cpp
@@ -3,32 +3,42 @@
 #include <atomicc.h>
 
 class Count {
+protected:
   Reg<int> *x;
   class Countup : public Rule {
+    Count *module;
+  public:
+    Countup(Count *module) : module(module) {}
     bool guard() {
-      return x.read() < 30;
+      return module->x->read() < 30;
     }
     void body() {
-      int y = x->read();
-      x->write(y + 1);
-      printf("x=%d, y=%d", x.read(), y);
+      int y = module->x->read();
+      module->x->write(y + 1);
+      printf("x=%d, y=%d", module->x->read(), y);
     }
   } countup;
 
   class Done : public Rule {
+    Count *module;
+  public:
+    Done(Count *module) : module(module) {}
     bool guard() {
-      return x.read() >= 30;
+      return module->x->read() >= 30;
     }
     void body() {
       exit(0);
     }
   } done;
+  friend class Countup;
+  friend class Done;
 public:
   Count();
 };
 
 Count::Count()
-  : x(new Reg<int>(0)) {
+  : x(new Reg<int>(0)), countup(this), done(this)
+{
 }
 
 Count countTest;
