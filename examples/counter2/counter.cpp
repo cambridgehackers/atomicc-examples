1cc675953d57a6935fa896f58807370cdcaa1a90
John Ankcorn <jca@alum.mit.edu>
2014-08-17 09:43:18 -0400

    make a readable IR file

--- examples/counter2/counter.cpp
+++ examples/counter2/counter.cpp
@@ -4,24 +4,30 @@
 
 #include <atomicc2.h>
 
-extern void append(std::function<bool ()> f1, std::function<void ()> f2);
+extern "C" void append(std::function<bool ()> guard, std::function<void ()> body, std::function<void ()> cleanup);
 
 void foo()
 {
   int x;
+  int y;
+  int temp_x;
+  int temp_y;
   x = 0;
   append([&](){ return x < 30;}, [&]()
   {
-      int temp_x;
-      int y = x;
-      x = y + 1;
+      int localy = x;
+      temp_x = y + 1;
+      temp_y = x;
       temp_x = y + 1;
       printf("x=%d, y=%d", x, y);
+  }, [&]()
+  {
       x = temp_x;
+      y = temp_y;
   });
   append([&](){ return x >= 30;}, [&]()
   {
       exit(0);
-  });
+  }, [&](){});
 }
 
