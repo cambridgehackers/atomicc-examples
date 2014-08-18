66ce782f3620061a6a05c8d99be563b35812678f
Jamey Hicks <jamey.hicks@qrclab.com>
2014-08-18 16:52:30 -0400

    added RULE macro

--- examples/counter/counter.cpp
+++ examples/counter/counter.cpp
@@ -2,48 +2,42 @@
 #include <stdlib.h>
 #include <atomicc.h>
 
+#define RULE(moduletype,name,guardbody,bodybody,updatebody) \
+  class name : public Rule {moduletype *module; public: name(moduletype *module=0) : module(module) {} virtual bool guard() guardbody; virtual void body() bodybody; void update() updatebody } name ## Instance;
+
 class Count {
 protected:
   Reg<int> x;
   int new_x;
-  class Countup : public Rule {
-    Count *module;
-  public:
-    Countup(Count *module) : module(module) {}
-    bool guard() {
-      return module->x < 30;
-    }
-    void body() {
-      int y = module->x;
-      module->new_x  = y + 1;
-      printf("x=%d, y=%d", (int)module->x, y);
-    }
-    void update() {
-      module->x = module->new_x;
-    }
-  } countup;
+  RULE(Count,count,
+       {
+	 return module->x < 30;
+       },
+       {
+	 int y = module->x;
+	 module->new_x  = y + 1;
+	 printf("x=%d, y=%d", (int)module->x, y);
+       },
+       {
+	 module->x = module->new_x;
+       })
 
-  class Done : public Rule {
-    Count *module;
-  public:
-    Done(Count *module) : module(module) {}
-    bool guard() {
-      return module->x >= 30;
-    }
-    void body() {
-    }
-    void update() {
-      exit(0);
-    }
-  } done;
-  friend class Countup;
-  friend class Done;
+  RULE(Count,done,
+       {
+	 return module->x >= 30;
+       },
+       {
+	 // no body
+       },
+       {
+	 exit(0);
+       })
 public:
   Count();
 };
 
 Count::Count()
-  : x(0), countup(this), done(this)
+  : x(0), countInstance(this), doneInstance(this)
 {
 }
 
