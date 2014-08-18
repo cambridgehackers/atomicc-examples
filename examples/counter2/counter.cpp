ccbeb7b93503341f3c41bf2a2acc08c1d2632272
John Ankcorn <jca@alum.mit.edu>
2014-08-18 08:46:00 -0400

    try using function pointers

--- examples/counter2/counter.cpp
+++ examples/counter2/counter.cpp
@@ -4,7 +4,13 @@
 //#include <functional>
 #include "functional_simple"
 
-extern "C" void append(std::function<bool()> guard, std::function<void()> body, std::function<void()> cleanup);
+typedef bool (*BFUN)();
+typedef void (*VFUN)();
+#define BF(A) std::function<bool()>(A).func2
+#define VF(A) std::function<void()>(A).func2
+
+//extern "C" void append(std::function<bool()> guard, std::function<void()> body, std::function<void()> cleanup);
+extern "C" void append(BFUN guard, VFUN body, VFUN cleanup);
 
 class Count {
 protected:
@@ -17,24 +23,25 @@ public:
 {
   x = 0;
 #if 1
-  append([this] () { return x < 30;},
-  [this] ()
+  append(BF([this] () { return x < 30;}),
+  VF([this] ()
   {
       int localy = x;
       temp_x = y + 1;
       temp_y = x;
       temp_x = y + 1;
       printf("x=%d, y=%d", x, y);
-  },
-  [this] ()
+  }),
+  VF([this] ()
   {
       x = temp_x;
       y = temp_y;
-  });
-  append([this] () { return x >= 30;}, [this] ()
+  }));
+  append(BF([this] () { return x >= 30;}),
+  VF([this] ()
   {
       exit(0);
-  }, [this](){});
+  }), VF([this](){}));
 #endif
 }
 
