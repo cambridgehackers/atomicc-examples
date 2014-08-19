78c341cc8dfb67b79e54edccb14030ad099b39c8
Jamey Hicks <jamey.hicks@qrclab.com>
2014-08-19 13:56:12 -0400

    fifo.h: action methods return Action *

--- cpp/fifo.h
+++ cpp/fifo.h
@@ -1,6 +1,7 @@
 #ifndef _FIFO_H_
 #define _FIFO_H_
 
+#include <assert.h>
 #include <atomicc.h>
 
 template<class T>
@@ -8,40 +9,62 @@ class Fifo : public Module {
  public:
   Fifo() {}
   virtual ~Fifo() {}
-  virtual void enq(T v) = 0;
+  virtual Action *enq(T &v) = 0;
   virtual T first() const = 0;
-  virtual void deq() = 0;
-  virtual bool notEmpty() = 0;
-  virtual bool notFull() = 0;
+  virtual Action * deq() = 0;
+  virtual bool notEmpty() const = 0;
+  virtual bool notFull() const = 0;
 };
 
 template<class T>
-class Fifo1 : public Fifo {
+class Fifo1 : public Fifo<T> {
   T element;
   bool full;
+
+  class EnqAction : public Action {
+    Fifo1 *fifo;
+    T &v;
+  public:
+  EnqAction(Fifo1 *fifo, T &v) : fifo(fifo), v(v) {}
+    bool guard() { return fifo->notFull(); }
+    void body() { }
+    void update() {
+      // ok, how do we get the value we wanted?
+      fifo->element = v;
+      fifo->full = true;
+    }
+  };
+  friend class EnqAction;
+  class DeqAction : public Action {
+    Fifo1 *fifo;
+    public:
+      DeqAction(Fifo1 *fifo) : fifo(fifo) {}
+      bool guard() { return fifo->notEmpty(); }
+      void body() { }
+      void update() {
+	fifo->full = false;
+      }
+    };
+  friend class DeqAction;
  public:
   Fifo1() {};
-  ~FIFO1() {}
+  ~Fifo1() {}
 
-  void enq(T v) {
-    // guarded atomic action
-    if (!full) {
-      element = v;
-      full = true;
-    }
+  bool notEmpty() const {
+    return full;
+  }
+  bool notFull() const {
+    return !full;
+  }
+  Action *enq(T &v) {
+    return new EnqAction(this, v);
   }
   T first() const {
-    // guarded expression
-    if (full)
-      return element;
-    else
-      fail;
+    assert(full);
+    return element;
   }
-  void deq() {
-    // guarded atomic action
-    if (full) {
-      full = false;
-    }
+  Action *deq() {
+    return new DeqAction(this);
   }
 };
 
