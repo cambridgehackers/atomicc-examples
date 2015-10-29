d9aff5c88e7791efa40047441571868a752a0cef
John Ankcorn <jca@alum.mit.edu>
2015-10-29 02:10:38 -0400

    cleanup

--- cpp/atomicc.h
+++ cpp/atomicc.h
@@ -1,3 +1,24 @@
+// Copyright (c) 2015 The Connectal Project
+
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use, copy,
+// modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 #ifndef _ATOMICC_H_
 #define _ATOMICC_H_
 
@@ -7,18 +28,20 @@
 #include <string.h>
 
 #define ACTION(A,B,C) \
-   virtual bool A ## __RDY() { return (C); } \
+   virtual bool A ## __RDY(void) { return (C); } \
    virtual void A B
 #define GVALUE(A,B,C) \
-   virtual bool A ## __RDY() { return (C); } \
+   virtual bool A ## __RDY(void) { return (C); } \
    virtual B A(void)
 
 class Rule {
 public:
-  virtual bool RDY() = 0;
-  virtual void ENA() = 0;
+  virtual bool RDY(void) = 0;
+  virtual void ENA(void) = 0;
   Rule *next;
 };
+typedef bool (Rule::*RDY_POINTER)(void);
+typedef void (Rule::*ENA_POINTER)(void);
 
 class Module {
  public:
