4dd3c6d6823b298e1e466b635b06f3fcb14e9fb5
John Ankcorn <jca@alum.mit.edu>
2018-02-19 11:12:07 -0800

    change representation of parameters in IR

---
+++ examples/echo2/ivector.generated.v
@@ -0,0 +1,161 @@
+`include "ivector.generated.vh"
+
+module l_module_OC_Fifo1_OC_3 (
+    input CLK,
+    input nRST,
+    input in$enq__ENA,
+    input [95:0]in$enq$v,
+    output in$enq__RDY,
+    input out$deq__ENA,
+    output out$deq__RDY,
+    output [95:0]out$first,
+    output out$first__RDY);
+    reg[95:0] element;
+    reg full;
+    assign in$enq__RDY = full ^ 1;
+    assign out$deq__RDY = full;
+    assign out$first = element;
+    assign out$first__RDY = full;
+
+    always @( posedge CLK) begin
+      if (!nRST) begin
+        element <= 0;
+        full <= 0;
+      end // nRST
+      else begin
+        if (in$enq__ENA) begin
+            element <= in$enq$v;
+            full <= 1;
+        end; // End of in$enq__ENA
+        if (out$deq__ENA) begin
+            full <= 0;
+        end; // End of out$deq__ENA
+      end
+    end // always @ (posedge CLK)
+endmodule 
+
+module l_module_OC_FifoPong (
+    input CLK,
+    input nRST,
+    input in$enq__ENA,
+    input [95:0]in$enq$v,
+    output in$enq__RDY,
+    input out$deq__ENA,
+    output out$deq__RDY,
+    output [95:0]out$first,
+    output out$first__RDY);
+    wire element1$in$enq__RDY;
+    wire element1$out$deq__RDY;
+    wire element1$out$first__RDY;
+    l_module_OC_Fifo1_OC_3 element1 (
+        CLK,
+        nRST,
+        in$enq__ENA & pong ^ 1,
+        in$enq$v,
+        element1$in$enq__RDY,
+        out$deq__ENA & pong ^ 1,
+        element1$out$deq__RDY,
+        out$first$retval,
+        element1$out$first__RDY);
+    wire element2$in$enq__RDY;
+    wire element2$out$deq__RDY;
+    wire [95:0]element2$out$first;
+    wire element2$out$first__RDY;
+    l_module_OC_Fifo1_OC_3 element2 (
+        CLK,
+        nRST,
+        in$enq__ENA & pong,
+        in$enq$v,
+        element2$in$enq__RDY,
+        out$deq__ENA & pong,
+        element2$out$deq__RDY,
+        element2$out$first,
+        element2$out$first__RDY);
+    reg pong;
+    assign in$enq__RDY = (element2$in$enq__RDY | (pong ^ 1)) & (element1$in$enq__RDY | pong);
+    assign out$deq__RDY = (element2$out$deq__RDY | (pong ^ 1)) & (element1$out$deq__RDY | pong);
+    assign out$first = out$first$retval;
+    assign out$first__RDY = (element2$out$first__RDY | (pong ^ 1)) & (element1$out$first__RDY | pong);
+
+    always @( posedge CLK) begin
+      if (!nRST) begin
+        pong <= 0;
+      end // nRST
+      else begin
+        if (out$deq__ENA) begin
+            pong <= pong ^ 1;
+        end; // End of out$deq__ENA
+      end
+    end // always @ (posedge CLK)
+endmodule 
+
+module l_module_OC_IVector (
+    input CLK,
+    input nRST,
+    input request$say__ENA,
+    input [95:0]request$say$v,
+    output request$say__RDY,
+    output ind$heard__ENA,
+    output [95:0]ind$heard$v,
+    input ind$heard__RDY);
+    wire fifo$out$deq__RDY;
+    wire fifo$out$first__RDY;
+    l_module_OC_FifoPong fifo (
+        CLK,
+        nRST,
+        request$say__ENA,
+        request$say$v,
+        request$say__RDY,
+        respond__ENA,
+        fifo$out$deq__RDY,
+        ind$heard$v,
+        fifo$out$first__RDY);
+    assign ind$heard__ENA = respond__ENA;
+    assign respond__RDY = (fifo$out$deq__RDY & fifo$out$first__RDY) & ind$heard__RDY;
+endmodule 
+
+module l_module_OC_Fifo1 (
+    input CLK,
+    input nRST,
+    input in$enq__ENA,
+    input [31:0]in$enq$v,
+    output in$enq__RDY,
+    input out$deq__ENA,
+    output out$deq__RDY,
+    output [31:0]out$first,
+    output out$first__RDY);
+    reg[31:0] element;
+    reg full;
+    assign in$enq__RDY = full ^ 1;
+    assign out$deq__RDY = full;
+    assign out$first = element;
+    assign out$first__RDY = full;
+
+    always @( posedge CLK) begin
+      if (!nRST) begin
+        element <= 0;
+        full <= 0;
+      end // nRST
+      else begin
+        if (in$enq__ENA) begin
+            element <= in$enq$v;
+            full <= 1;
+        end; // End of in$enq__ENA
+        if (out$deq__ENA) begin
+            full <= 0;
+        end; // End of out$deq__ENA
+      end
+    end // always @ (posedge CLK)
+endmodule 
+
+module l_module_OC_IVectorIndication (
+    input CLK,
+    input nRST,
+    input ind$heard__ENA,
+    input [95:0]ind$heard$v,
+    output ind$heard__RDY);
+    assign ind$heard__RDY = 1;
+    // Extra assigments, not to output wires
+    assign ind$heard__ENA$v_2e_addr = ind$heard$v;
+endmodule 
+
---
+++ examples/echo2/ivector.generated.vh
@@ -0,0 +1,37 @@
+`ifndef __ivector_GENERATED__VH__
+`define __ivector_GENERATED__VH__
+
+//METASTART; l_module_OC_Fifo1_OC_3
+//METAEXCLUSIVE; in$enq__ENA; out$deq__ENA
+//METAGUARD; in$enq; full ^ 1;
+//METAGUARD; out$deq; full;
+//METABEFORE; out$first; :in$enq__ENA
+//METAGUARD; out$first; full;
+//METASTART; l_module_OC_FifoPong
+//METAINTERNAL; element1; l_module_OC_Fifo1_OC_3;
+//METAINTERNAL; element2; l_module_OC_Fifo1_OC_3;
+//METAINVOKE; in$enq__ENA; pong ^ 1:element1$in$enq__ENA;pong:element2$in$enq__ENA;
+//METABEFORE; in$enq__ENA; :out$deq__ENA
+//METAGUARD; in$enq; (element2$in$enq__RDY | (pong ^ 1)) & (element1$in$enq__RDY | pong);
+//METAINVOKE; out$deq__ENA; pong ^ 1:element1$out$deq__ENA;pong:element2$out$deq__ENA;
+//METAGUARD; out$deq; (element2$out$deq__RDY | (pong ^ 1)) & (element1$out$deq__RDY | pong);
+//METAINVOKE; out$first; pong ^ 1:element1$out$first;pong:element2$out$first;
+//METABEFORE; out$first; :out$deq__ENA
+//METAGUARD; out$first; (element2$out$first__RDY | (pong ^ 1)) & (element1$out$first__RDY | pong);
+//METASTART; l_module_OC_IVector
+//METAEXTERNAL; ind; l_ainterface_OC_IndIF;
+//METAINTERNAL; fifo; l_module_OC_FifoPong;
+//METAINVOKE; request$say__ENA; :fifo$in$enq__ENA;
+//METAGUARD; request$say; fifo$in$enq__RDY;
+//METAINVOKE; respond__ENA; :fifo$out$deq__ENA;:fifo$out$first;:ind$heard__ENA;
+//METAGUARD; respond; (fifo$out$deq__RDY & fifo$out$first__RDY) & ind$heard__RDY;
+//METARULES; respond
+//METASTART; l_module_OC_Fifo1
+//METAEXCLUSIVE; in$enq__ENA; out$deq__ENA
+//METAGUARD; in$enq; full ^ 1;
+//METAGUARD; out$deq; full;
+//METABEFORE; out$first; :in$enq__ENA
+//METAGUARD; out$first; full;
+//METASTART; l_module_OC_IVectorIndication
+//METAGUARD; ind$heard; 1;
+`endif
