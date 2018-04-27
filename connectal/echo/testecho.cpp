20ac59cf51d1b8515ded114204fc6d9634f6cb1c
John Ankcorn <jca@alum.mit.edu>
2018-04-27 10:21:28 -0700

    make sure vlsim is running at start of ubuntu.exe/main()

--- connectal/echo/testecho.cpp
+++ connectal/echo/testecho.cpp
@@ -63,6 +63,7 @@ int main(int argc, const char **argv)
     long actualFrequency = 0;
     long requestedFrequency = 1e9 / MainClockPeriod;
 
+    initPortalHardware();  // start vlsim running
 #if 0
     EchoIndication echoIndication(IfcNames_EchoIndicationH2S, NULL, NULL);
     echoRequestProxy = new EchoRequestProxy(IfcNames_EchoRequestS2H);
