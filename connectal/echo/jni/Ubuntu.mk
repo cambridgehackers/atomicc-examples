9e7c18ccff9be1df9a19745de1b18df05856c80d
John Ankcorn <jca@alum.mit.edu>
2018-04-29 13:57:48 -0700

    cleanup and changed names in connectal/echo example back to request/indication for interfaces

--- connectal/echo/jni/Ubuntu.mk
+++ connectal/echo/jni/Ubuntu.mk
@@ -7,7 +7,13 @@ CXX=$(TOOLCHAIN)g++
 CFLAGS_COMMON = -O -g -I$(DTOP)/jni -I$(CONNECTALDIR) -I$(CONNECTALDIR)/cpp
 CFLAGS = $(CFLAGS_COMMON)
 
-include $(DTOP)/../scripts/Makefile.connectal.application
+#include $(DTOP)/../scripts/Makefile.connectal.application
+include $(DTOP)/jni/Makefile.generated_files
+PORTAL_INFRA := portal.c transportHardware.c transportSocket.c portalPrintf.c poller.cpp sock_utils.c timer.c
+#PORTAL_INFRA += transportShared.c transportSerial.c portalJson.c 
+PORTAL_SRC_FILES := $(addprefix $(CONNECTALDIR)/cpp/, $(PORTAL_INFRA)) \
+      $(addprefix $(DTOP)/jni/,  $(GENERATED_CPP))
+
 SOURCES = ../testecho.cpp $(PORTAL_SRC_FILES)
 LDLIBS :=    -lpthread
 
--- connectal/echo/verilog/mkVsimTop.v
+++ connectal/echo/verilog/mkVsimTop.v
@@ -15,12 +15,12 @@ module mkVsimTop(input CLK_derivedClock, input RST_N_derivedReset, input CLK_sys
     .data_v(outgoingData), .data_length(outgoingLength),
     .EN_data(EN_outgoing && RDY_outgoing), .RDY_data(RDY_outgoing));
 
-  mkEcho echo(.CLK(CLK), .RST_N(RST_N),
-    .in_enq_v({16'b0, incomingData[`MAX_IN_WIDTH-1:16]}),
-    .EN_in_enq(RDY_incoming && incomingData[15:0] == 16'd6 && RDY_echo_in_enq),
-    .RDY_in_enq(RDY_echo_in_enq),
-    .out_enq_v(echoData),
-    .EN_out_enq(EN_echo_out_enq), .RDY_out_enq(RDY_echo_out_enq));
+  mkEcho top(.CLK(CLK), .RST_N(RST_N),
+    .request_enq_v({16'b0, incomingData[`MAX_IN_WIDTH-1:16]}),
+    .EN_request_enq(RDY_incoming && incomingData[15:0] == 16'd6 && RDY_echo_in_enq),
+    .RDY_request_enq(RDY_echo_in_enq),
+    .indication_enq_v(echoData),
+    .EN_indication_enq(EN_echo_out_enq), .RDY_indication_enq(RDY_echo_out_enq));
 
   assign EN_incoming = RDY_echo_in_enq;  // 'and' together all RDY signals
   assign EN_outgoing = (EN_echo_out_enq && RDY_echo_out_enq); // 'or' together all EN signals
