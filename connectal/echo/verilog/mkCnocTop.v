d5592f64372b4ae76b20d48cfdf14dc6a1c98788
John Ankcorn <jca@alum.mit.edu>
2018-04-28 20:53:11 -0700

    change CnocTop outgoing interface to be an outcall to a PipeIn, instead of PipeOut called from outside

--- connectal/echo/verilog/mkCnocTop.v
+++ connectal/echo/verilog/mkCnocTop.v
@@ -1,32 +1,32 @@
 
 `include "ProjectDefines.vh"
 module mkCnocTop( input  CLK, input  RST_N,
-  input  [`MAX_IN_WIDTH -1: 0] in_enq_v, input  EN_in_enq, output RDY_in_enq,
-  output [`MAX_OUT_WIDTH -1: 0] out_first, output RDY_out_first,
-  input  EN_out_deq, output RDY_out_deq);
+  input  [`MAX_IN_WIDTH -1: 0] in_enq_v, input EN_in_enq, output RDY_in_enq,
+  output [`MAX_OUT_WIDTH -1: 0] out_enq_v, output EN_out_enq, input RDY_out_enq);
 
-  wire indAvail;
+  wire outputEnable;
   wire [31:0] lEcho_heard_v, lEcho_heard2_a_b;
   wire lEcho_delay_FULL_N, lEcho_delay2_FULL_N, lEcho_delay_EMPTY_N, lEcho_delay2_EMPTY_N;
   wire [`MAX_OUT_WIDTH -1: 0] outputValue;
 
   assign RDY_in_enq = lEcho_delay_FULL_N && lEcho_delay2_FULL_N;
-  assign indAvail = lEcho_delay_EMPTY_N || lEcho_delay2_EMPTY_N;
-  assign RDY_out_first = indAvail;
-  assign RDY_out_deq = indAvail;
-  assign out_first = outputValue;
-  // value, 16'd(methodNumber), 16'd(length)
-  assign outputValue = lEcho_delay_EMPTY_N ? {lEcho_heard_v, 16'd0, 16'd1} : {lEcho_heard2_a_b, 16'd1, 16'd1};
+  assign EN_out_enq = outputEnable;
+  assign out_enq_v = outputValue;
+
+  assign outputEnable = RDY_out_enq && (lEcho_delay_EMPTY_N || lEcho_delay2_EMPTY_N);
+                                             // value, 16'd(methodNumber), 16'd(length)
+  assign outputValue = lEcho_delay_EMPTY_N ? {lEcho_heard_v, 16'd0, 16'd1} :
+                                             {lEcho_heard2_a_b, 16'd1, 16'd1};
 
   SizedFIFO #(.p1width(32'd32), .p2depth(32'd8), .p3cntr_width(32'd3), .guarded(32'd1)) lEcho_delay(
       .RST(RST_N), .CLK(CLK), .D_IN(in_enq_v[47:16]),
       .ENQ(EN_in_enq && in_enq_v[15:0] == 0 && lEcho_delay_FULL_N),
-      .DEQ(EN_out_deq && outputValue[31:16] == 0 && lEcho_delay_EMPTY_N), .CLR(0),
+      .DEQ(outputEnable && outputValue[31:16] == 0 && lEcho_delay_EMPTY_N), .CLR(0),
       .D_OUT(lEcho_heard_v), .FULL_N(lEcho_delay_FULL_N), .EMPTY_N(lEcho_delay_EMPTY_N));
 
   SizedFIFO #(.p1width(32'd32), .p2depth(32'd8), .p3cntr_width(32'd3), .guarded(32'd1)) lEcho_delay2(
       .RST(RST_N), .CLK(CLK), .D_IN(in_enq_v[47:16]),
       .ENQ(EN_in_enq && in_enq_v[15:0] == 1 && lEcho_delay2_FULL_N),
-      .DEQ((EN_out_deq && outputValue[31:16] == 1) && lEcho_delay2_EMPTY_N), .CLR(0),
+      .DEQ((outputEnable && outputValue[31:16] == 1) && lEcho_delay2_EMPTY_N), .CLR(0),
       .D_OUT(lEcho_heard2_a_b), .FULL_N(lEcho_delay2_FULL_N), .EMPTY_N(lEcho_delay2_EMPTY_N));
 endmodule
--- connectal/echo/verilog/mkVsimTop.v
+++ connectal/echo/verilog/mkVsimTop.v
@@ -1,9 +1,9 @@
 
 `include "ProjectDefines.vh"
 module mkVsimTop(input CLK_derivedClock, input RST_N_derivedReset, input CLK_sys_clk, input CLK, input RST_N);
-  wire sink_0_RDY_beat, nextInBeat, nextOutBeat, sinkLast, sourceLast;
+  wire sink_0_RDY_beat, nextInBeat, sinkLast, sourceLast;
   wire [31 : 0] beatInData;
-  wire EIRDY_in_enq, EIRDY_out_first, EIRDY_out_deq;
+  wire RDY_in_enq, EN_out_enq;
   wire [`MAX_OUT_WIDTH-1 : 0]indData;
 
   reg [`MAX_OUT_WIDTH-1 : 0] outgoingData;
@@ -12,19 +12,17 @@ module mkVsimTop(input CLK_derivedClock, input RST_N_derivedReset, input CLK_sys
   reg [15 : 0] indicationWords;
 
   VsimSink sink_0(.RST(RST_N), .CLK(CLK), .CLK_GATE(1'd1),
-    .EN_beat(sink_0_RDY_beat && EIRDY_in_enq),
+    .EN_beat(sink_0_RDY_beat && RDY_in_enq),
     .beat(beatInData), .RDY_beat(sink_0_RDY_beat), .last(sinkLast));
   VsimSource tmp_1_0(.RST(RST_N), .CLK(CLK), .CLK_GATE(1'd1),
-    .beat(outgoingData[31:0]), .last(sourceLast), .en_beat(nextOutBeat && indicationState));
+    .beat(outgoingData[31:0]), .last(sourceLast), .en_beat(indicationState));
 
   mkCnocTop top(.CLK(CLK), .RST_N(RST_N),
       .in_enq_v({incomingData[`MAX_IN_WIDTH-1-16:0], beatInData[31:16]}),
-      .EN_in_enq(nextInBeat && sinkLast && EIRDY_in_enq), .RDY_in_enq(EIRDY_in_enq),
-      .out_first(indData), .RDY_out_first(EIRDY_out_first),
-      .EN_out_deq(sourceLast && EIRDY_out_deq), .RDY_out_deq(EIRDY_out_deq));
+      .EN_in_enq(nextInBeat && sinkLast), .RDY_in_enq(RDY_in_enq),
+      .out_enq_v(indData), .RDY_out_enq(!indicationState), .EN_out_enq(EN_out_enq));
 
-  assign nextInBeat = sink_0_RDY_beat && EIRDY_in_enq;
-  assign nextOutBeat = EIRDY_out_first && EIRDY_out_deq;
+  assign nextInBeat = sink_0_RDY_beat && RDY_in_enq;
   assign sourceLast = indicationWords == 16'd1;
 
   always@(posedge CLK)
@@ -40,19 +38,17 @@ module mkVsimTop(input CLK_derivedClock, input RST_N_derivedReset, input CLK_sys
           //$display("CNOCTOP: incoming %x last %x incomingData %x", beatInData, sinkLast, incomingData);
           incomingData = {incomingData[`MAX_IN_WIDTH-1-32:0], beatInData};
         end
-        if (nextOutBeat) begin
-          if (!indicationState) begin
-              indicationState <= 1;
-              outgoingData = {indData[`MAX_OUT_WIDTH-1:16], /*portal*/16'd5};
-              indicationWords <= indData[15:0] + 1;
-          end
-          else begin
-              //$display("CNOCTOP: state %x outgoing last %x data %x", indicationState, sourceLast, outgoingData);
-              outgoingData = {32'b0, outgoingData[`MAX_IN_WIDTH-1:32]};
-              indicationWords <= indicationWords - 16'd1;
-              if (sourceLast)
-                indicationState <= 0;
-          end
+        if (EN_out_enq) begin
+            indicationState <= 1;
+            outgoingData = {indData[`MAX_OUT_WIDTH-1:16], /*portal*/16'd5};
+            indicationWords <= indData[15:0] + 1;
+        end
+        if (indicationState) begin
+            //$display("CNOCTOP: state %x outgoing last %x data %x", indicationState, sourceLast, outgoingData);
+            outgoingData = {32'b0, outgoingData[`MAX_IN_WIDTH-1:32]};
+            indicationWords <= indicationWords - 16'd1;
+            if (sourceLast)
+              indicationState <= 0;
         end
       end
   end
