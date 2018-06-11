c5a6a4e5df202edc55d494a8b4ca467bc9fe9607
John Ankcorn <jca@alum.mit.edu>
2018-06-11 14:22:11 -0700

    refactor processing in VsimSink/Source with mkVsimTop.v

--- examples/rulec/verilog/mkVsimTop.v
+++ examples/rulec/verilog/mkVsimTop.v
@@ -1,19 +1,56 @@
 
 `include "ProjectDefines.vh"
 module mkVsimTop(input CLK_derivedClock, input RST_N_derivedReset, input CLK_sys_clk, input CLK, input RST_N);
-  wire RDY_incoming, EN_incoming;
+  wire EN_incoming, EN_outgoing, sinkLast, EN_sinkBeat, EN_sourceBeat, sourceLast;
   wire RDY_echo_in_enq, EN_echo_out_enq, RDY_echo_out_enq, incomingEnable;
   wire [`MAX_OUT_WIDTH-1 : 0] outgoingData, echoData;
-  wire [`MAX_IN_WIDTH-1 : 0] incomingData;
   wire [15: 0] outgoingLength;
-  wire RDY_outgoing, EN_outgoing;
+  wire [`MAX_BUS_WIDTH-1:0] sinkData;
 
-  VsimSink#(.width(`MAX_IN_WIDTH)) sink_0(.RST_N(RST_N), .CLK(CLK),
-    .data(incomingData),
-    .EN_data(EN_incoming && RDY_incoming), .RDY_data(RDY_incoming));
-  VsimSource#(.width(`MAX_IN_WIDTH)) source_0(.RST_N(RST_N), .CLK(CLK),
-    .data_v(outgoingData), .data_length(outgoingLength),
-    .EN_data(EN_outgoing && RDY_outgoing), .RDY_data(RDY_outgoing));
+  reg  RDY_outgoing, RDY_incoming;
+  reg  [`MAX_IN_WIDTH-1 : 0] incomingData;
+  reg  [`MAX_OUT_WIDTH-1 : 0] outgoingBuffer;
+  reg  [15 : 0] indicationWords;
+
+  assign sourceLast = indicationWords == 16'd1;
+
+  VsimSink #(.width(`MAX_BUS_WIDTH)) sink_0 (.CLK(CLK), .RST_N(RST_N),
+   .EN_beat(EN_sinkBeat), .RDY_beat(!RDY_incoming), .beat(sinkData), .last(sinkLast));
+
+  VsimSource #(.width(`MAX_BUS_WIDTH)) source_0 (.CLK(CLK), .RST_N(RST_N),
+   .EN_beat(EN_sourceBeat), .RDY_beat(!RDY_outgoing), .beat(outgoingBuffer[31:0]), .last(sourceLast));
+
+  always @(posedge CLK) begin
+     if (RST_N == `BSV_RESET_VALUE) begin
+        RDY_incoming <= 0;
+        RDY_outgoing <= 1;
+     end
+     else begin
+        // process 'write' requests
+        if (EN_incoming && RDY_incoming)
+            RDY_incoming <= 0;
+        if (EN_sinkBeat && !RDY_incoming) begin
+            $display("VSIMSINK: incomingData %x sinkData %x sinkLast %x", incomingData, sinkData, sinkLast);
+            incomingData <= incomingData << 32 | sinkData;
+            if (sinkLast)
+                RDY_incoming <= 1;
+        end 
+        // process 'read' requests
+        if (EN_outgoing && RDY_outgoing) begin
+            RDY_outgoing <= 0;
+            outgoingBuffer <= outgoingData;
+            indicationWords <= outgoingLength + 1;
+            //$display("VSOURCE: start data %x", outgoingData);
+        end
+        if (EN_sourceBeat && !RDY_outgoing) begin
+            //$display("VSOURCE: outgoing data %x last %x", outgoingBuffer, sourceLast);
+            outgoingBuffer <= {32'b0, outgoingBuffer[`MAX_OUT_WIDTH-1:32]};
+            if (sourceLast)
+                RDY_outgoing <= 1;
+            indicationWords <= indicationWords - 16'd1;
+        end
+    end
+  end
 
   l_top top(.CLK(CLK), .nRST(RST_N),
     .request$enq$v(incomingData),
