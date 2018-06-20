6bc16530e431eead49bcca5a752920a29d54ba8a
John Ankcorn <jca@alum.mit.edu>
2018-06-19 18:37:12 -0700

    cleanup mkVsimTop

--- examples/rulec/verilog/mkVsimTop.v
+++ examples/rulec/verilog/mkVsimTop.v
@@ -1,38 +1,30 @@
 
 `include "ProjectDefines.vh"
 module mkVsimTop(input CLK_derivedClock, input RST_N_derivedReset, input CLK_sys_clk, input CLK, input RST_N);
-  wire EN_incoming, EN_in$enqr, in$enq$last, EN_in$enq, EN_out$enqr, out$enq$last;
-  wire RDY_echo_in_enq, EN_echo_out_enq, RDY_echo_out_enq, incomingEnable;
-  wire [`MAX_OUT_WIDTH-1 : 0] in$enqData, echoData, incomingData;
-  wire [15: 0] in$enq$length;
-  wire [`MAX_BUS_WIDTH-1:0] in$enq$v, readDatar;
-  wire RDY_incoming, RDY_in$enqr;
+  wire EN_writeBeat, RDY_writeBeat, writeLast;
+  wire EN_readBeat, RDY_readBeat, readLast;
+  wire [`MAX_BUS_WIDTH-1:0] writeData, readData;
+  wire EN_incoming;
+  wire [`MAX_OUT_WIDTH-1 : 0] outgoingData, indicationData, requestData;
+  wire EN_request, RDY_request, EN_indication, RDY_indication;
 
   VsimReceive #(.width(`MAX_BUS_WIDTH)) sink_0 (.CLK(CLK), .nRST(RST_N),
-   .EN_beat(EN_in$enq), .RDY_beat(EN_in$enq), .beat(in$enq$v), .last(in$enq$last));
-
+   .EN_beat(EN_writeBeat), .RDY_beat(RDY_writeBeat), .beat(writeData), .last(writeLast));
   VsimSend #(.width(`MAX_BUS_WIDTH)) source_0 (.CLK(CLK), .nRST(RST_N),
-   .EN_beat(EN_out$enqr), .RDY_beat(!RDY_in$enqr), .beat(readDatar), .last(out$enq$last));
+   .EN_beat(EN_readBeat), .RDY_beat(RDY_readBeat), .beat(readData), .last(readLast));
 
   l_module_OC_AdapterFromBus wadapter_0(.CLK(CLK), .nRST(RST_N),
-    .in$enq__ENA(EN_in$enq), .in$enq$v(in$enq$v), .in$enq$last(in$enq$last), .in$enq__RDY(EN_in$enq),
-    .out$enq__ENA(EN_incoming), .out$enq$v(incomingData), .out$enq$length(), .out$enq__RDY(RDY_incoming));
+    .in$enq__ENA(EN_writeBeat), .in$enq__RDY(RDY_writeBeat), .in$enq$v(writeData), .in$enq$last(writeLast),
+    .out$enq__ENA(EN_incoming), .out$enq__RDY(RDY_request), .out$enq$v(requestData), .out$enq$length());
   l_module_OC_AdapterToBus radapter_0(.CLK(CLK), .nRST(RST_N),
-   .in$enq__ENA(EN_in$enqr), .in$enq__RDY(RDY_in$enqr), .in$enq$v(in$enqData), .in$enq$length(in$enq$length),
-   .out$enq__ENA(EN_out$enqr), .out$enq__RDY(!RDY_in$enqr), .out$enq$v(readDatar), .out$enq$last(out$enq$last));
+   .in$enq__ENA(EN_indication), .in$enq__RDY(RDY_indication), .in$enq$v(outgoingData), .in$enq$length(indicationData[15:0]),
+   .out$enq__ENA(EN_readBeat), .out$enq__RDY(RDY_readBeat), .out$enq$v(readData), .out$enq$last(readLast));
 
   l_top top(.CLK(CLK), .nRST(RST_N),
-    .request$enq$v(incomingData),
-    .request$enq__ENA(incomingEnable), .request$enq__RDY(RDY_echo_in_enq),
-    .indication$enq$v(echoData),
-    .indication$enq__ENA(EN_echo_out_enq), .indication$enq__RDY(RDY_echo_out_enq));
+    .request$enq__ENA(EN_request), .request$enq__RDY(RDY_request), .request$enq$v(requestData),
+    .indication$enq__ENA(EN_indication), .indication$enq__RDY(RDY_indication), .indication$enq$v(indicationData));
 
-  assign RDY_incoming = RDY_echo_in_enq;  // 'and' together all RDY signals
-  assign incomingEnable = EN_incoming && RDY_incoming && incomingData[15:0] == `IfcNames_EchoRequestS2H && RDY_echo_in_enq;
-
-  assign EN_in$enqr = RDY_in$enqr && (EN_echo_out_enq && RDY_echo_out_enq); // 'or' together all EN signals
-  // use priority encoder to mux out going signals when multiple portals
-  assign in$enqData = {echoData[`MAX_OUT_WIDTH-1:16], `IfcNames_EchoIndicationH2S};
-  assign in$enq$length = echoData[15:0];
-  assign RDY_echo_out_enq = RDY_in$enqr;
+  assign EN_request = EN_incoming && requestData[15:0] == `IfcNames_EchoRequestS2H;
+  assign outgoingData = {indicationData[`MAX_OUT_WIDTH-1:16], `IfcNames_EchoIndicationH2S};
+  assign RDY_readBeat = !RDY_indication;
 endmodule  // mkVsimTop
