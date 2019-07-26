0c3669bf880be279553cbebfa26f1cc373fc5d9d
John Ankcorn <jca@alum.mit.edu>
2019-07-26 13:22:39 +0900

    add support for 'instantiate for' in atomicc backend

--- lib/generated/FunnelHalfBase.v
+++ lib/generated/FunnelHalfBase.v
@@ -13,16 +13,30 @@ module FunnelHalfBase #(
     input wire output$enq__RDY);
     reg [dataWidth - 1:0]i;
     reg [dataWidth - 1:0]k;
+    genvar __inst$Genvar1;
     assign input$enq__RDY = 1;
     assign output$enq$v = 0; //MISSING_ASSIGNMENT_FOR_OUTPUT_VALUE
     assign output$enq__ENA = 0; //MISSING_ASSIGNMENT_FOR_OUTPUT_VALUE
+for(__inst$Genvar1 = 0; __inst$Genvar1 < ( funnelWidth / 2 ); __inst$Genvar1 = __inst$Genvar1 + 1) begin
+        assign output[__inst$Genvar1].enq$v = v;
+        assign output[__inst$Genvar1].enq__ENA = input$enq__ENA[ ( __inst$Genvar1 * 2 ) + 1 ] input$enq__ENA input$enq__ENA input$enq__ENA __ENA && input$enq__ENA[ ( __inst$Genvar1 * 2 ) + 1 ] input$enq__ENA input$enq__ENA input$enq__ENA __RDY;
+    end;
+
+    for(__inst$Genvar1 = 0; __inst$Genvar1 < ( funnelWidth / 2 ); __inst$Genvar1 = __inst$Genvar1 + 1) begin
 
     always @( posedge CLK) begin
       if (!nRST) begin
         i <= 0;
         k <= 0;
       end // nRST
+      else begin
+        if (input$enq__ENA[ ( __inst$Genvar1 * 2 ) + 1 ] input$enq__ENA input$enq__ENA input$enq__ENA __ENA && input$enq__ENA[ ( __inst$Genvar1 * 2 ) + 1 ] input$enq__ENA input$enq__ENA input$enq__ENA __RDY) begin // input$enq__ENA[( __inst$Genvar1 * 2 ) + 1]
+            i <= v;
+            k <= __inst$Genvar1;
+        end; // End of input$enq__ENA[( __inst$Genvar1 * 2 ) + 1]
+      end
     end // always @ (posedge CLK)
+   end // end of forloop
 endmodule 
 
 `default_nettype wire    // set back to default value
