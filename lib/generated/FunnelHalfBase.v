2189a82e214064e929bd5858a061dfdc3c6ea587
John Ankcorn <jca@alum.mit.edu>
2019-08-04 23:21:54 +0900

    push array size expressions to IR

--- lib/generated/FunnelHalfBase.v
+++ lib/generated/FunnelHalfBase.v
@@ -8,9 +8,9 @@ module FunnelHalfBase #(
     input wire input$enq__ENA[funnelWidth - 1:0],
     input wire [funnelWidth * dataWidth - 1:0]input$enq$v,
     output wire input$enq__RDY[funnelWidth - 1:0],
-    output wire output$enq__ENA[5 - 1:0],
-    output wire [5 * dataWidth - 1:0]output$enq$v,
-    input wire output$enq__RDY[5 - 1:0]);
+    output wire output$enq__ENA[funnelWidth / 2 - 1:0],
+    output wire [funnelWidth / 2 * dataWidth - 1:0]output$enq$v,
+    input wire output$enq__RDY[funnelWidth / 2 - 1:0]);
     genvar __inst$Genvar1;
 for(__inst$Genvar1 = 0; __inst$Genvar1 < ( funnelWidth / 2 ); __inst$Genvar1 = __inst$Genvar1 + 1) begin
         assign input$enq__RDY[__inst$Genvar1 * 2] = ( input$enq__ENA[ ( __inst$Genvar1 * 2 ) + 1 ] == 0 ) && output$enq__RDY[ __inst$Genvar1 ];
