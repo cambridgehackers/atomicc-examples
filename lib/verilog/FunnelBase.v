358b29e1fb1b7563a5f2f59450332e392caafa11
John Ankcorn <jca@alum.mit.edu>
2020-05-25 15:37:29 -0700

    revert back to previous version of verilog funnel code.
    Since C++ does not support declaration of arrays with elements of different sizes (verilog does!), we are abandoning the C++ version.

--- lib/verilog/FunnelBase.v
+++ lib/verilog/FunnelBase.v
@@ -20,6 +20,26 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+module pipeFunnelHalf #(parameter funnelWidth = 8, parameter dataWidth = 32) ( input CLK, input nRST,
+    input input$enq__ENA [funnelWidth-1:0],        /* array of inputs */
+    input [funnelWidth * dataWidth-1:0] input$enq$v,
+    output  input$enq__RDY [funnelWidth-1: 0],
+    output output$enq__ENA [funnelWidth/2 - 1: 0], /* merged output */
+    output [(funnelWidth /2) * dataWidth-1:0] output$enq$v,
+    input output$enq__RDY [funnelWidth/2 - 1: 0]);
+
+    genvar j;
+    for (j = 0; j < funnelWidth / 2; j = j+1) begin
+        wire rvalid = input$enq__ENA[j*2+1];
+        assign input$enq__RDY[j*2+1] = output$enq__RDY[j];
+        assign input$enq__RDY[j*2] = !rvalid && output$enq__RDY[j];
+        assign output$enq$v[dataWidth * (j + 1) - 1: dataWidth * j] =
+            rvalid ? input$enq$v[dataWidth * (j*2 + 2) - 1: dataWidth * (j*2 + 1)]:
+                     input$enq$v[dataWidth * (j*2 + 1) - 1: dataWidth * j*2];
+        assign output$enq__ENA[j] = rvalid | input$enq__ENA[j*2];
+    end
+endmodule
+//////////////////
 module FunnelBase #(parameter funnelWidth = 8, parameter dataWidth = 32) ( input CLK, input nRST,
     input input$enq__ENA[funnelWidth-1:0], /* array of inputs */
     input [funnelWidth * dataWidth-1:0] input$enq$v,
@@ -29,20 +49,22 @@ module FunnelBase #(parameter funnelWidth = 8, parameter dataWidth = 32) ( input
     input output$enq__RDY);
 
     genvar i;
-    localparam depth = $clog2(funnelWidth);
+    localparam depth = $clog2(funnelWidth) - 1;
     for(i = 0; i <= depth; i = i+1) begin : level
-       wire valid [funnelWidth/2**(i) - 1: 0];
-       wire [funnelWidth/2**(i) * dataWidth-1:0] data;
-       wire ready [funnelWidth/2**(i) - 1: 0];
+       wire valid [funnelWidth/2**(i+1) - 1: 0];
+       wire [funnelWidth/2**(i+1) * dataWidth-1:0] data;
+       wire ready [funnelWidth/2**(i+1) - 1: 0];
        if (i == 0)
-           valid = input$enq__ENA;
-           data = input$enq$v;
-           ready = input$enq__RDY;
+       pipeFunnelHalf #(funnelWidth/2**i, dataWidth) funnel(CLK, nRST,
+           input$enq__ENA, input$enq$v, input$enq__RDY, valid, data, ready);
        else
-       pipeFunnelHalf #(funnelWidth/2**(i-1), dataWidth) funnel(CLK, nRST,
+       pipeFunnelHalf #(funnelWidth/2**i, dataWidth) funnel(CLK, nRST,
            level[i-1].valid, level[i-1].data, level[i-1].ready, valid, data, ready);
     end
+    assign output$enq$v = level[depth].data[dataWidth - 1: 0];
     assign output$enq__ENA = level[depth].valid[0];
-    assign output$enq$v = level[depth].data;//[dataWidth - 1: 0];
     assign level[depth].ready[0] = output$enq__RDY;
 endmodule
+
+
+
