6953117cb391b2101c07bd0c561ce698045b567d
John Ankcorn <jca@alum.mit.edu>
2020-06-15 20:26:26 -0700

    add syncFF.cpp

---
+++ lib/generated/SyncFF.v
@@ -0,0 +1,27 @@
+`include "syncFF.generated.vh"
+
+`default_nettype none
+module SyncFF (
+    input wire CLK,
+    input wire nRST,
+    output wire out,
+    input wire in);
+    reg one;
+    reg two;
+    assign out = two;
+
+    always @( posedge CLK) begin
+      if (!nRST) begin
+        one <= 0;
+        two <= 0;
+      end // nRST
+      else begin
+        // RULE$init__ENA
+            one <= in;
+            two <= one;
+        // End of RULE$init__ENA
+      end
+    end // always @ (posedge CLK)
+endmodule 
+
+`default_nettype wire    // set back to default value
---
+++ lib/generated/syncFF.generated.vh
@@ -0,0 +1,7 @@
+`ifndef __syncFF_GENERATED__VH__
+`define __syncFF_GENERATED__VH__
+
+//METASTART; SyncFF
+//METAGUARD; RULE$init; 1;
+//METARULES; RULE$init
+`endif
