5894867d411a060d2e1a2f007fd2d69d329a24a8
John Ankcorn <jca@alum.mit.edu>
2020-08-25 11:34:35 -0700

    updated vsimtop

--- lib/generated/VsimTop.sv
+++ lib/generated/VsimTop.sv
@@ -7,15 +7,15 @@ module VsimTop (
     input wire CLK_derivedClock,
     input wire nRST_derivedReset,
     input wire CLK_sys_clk);
-    PipeInB#(.width(32)) source_0$port();
-    PipeInB#(.width(32)) user$write();
+    PipeInB#(.width(32)) sink_0$port();
+    PipeInB#(.width(32)) user$read();
     UserTop user (.CLK(CLK), .nRST(nRST),
-        .write(user$write),
-        .read(source_0$port));
+        .write(sink_0$port),
+        .read(user$read));
     VsimReceive#(.width(32)) sink_0 (.CLK(CLK), .nRST(nRST),
-        .port(user$write));
+        .port(sink_0$port));
     VsimSend#(.width(32)) source_0 (.CLK(CLK), .nRST(nRST),
-        .port(source_0$port));
+        .port(user$read));
 endmodule
 
 `default_nettype wire    // set back to default value
