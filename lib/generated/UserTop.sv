190f256cc4fa78a18bf0eea3de8c74155f4fe42a
John Ankcorn <jca@alum.mit.edu>
2020-08-25 11:32:52 -0700

    updated usertop

--- lib/generated/UserTop.sv
+++ lib/generated/UserTop.sv
@@ -4,17 +4,17 @@
 module UserTop (input wire CLK, input wire nRST,
     PipeInB.server write,
     PipeInB.client read);
-    PipeIn#(.width(16 + 128)) ctop$request();
-    PipeIn#(.width(16 + 128)) radapter_0$in();
+    PipeIn#(.width(16 + 128)) ctop$indication();
+    PipeIn#(.width(16 + 128)) wadapter_0$out();
     AdapterToBus#(.width(32)) radapter_0 (.CLK(CLK), .nRST(nRST),
-        .in(radapter_0$in),
+        .in(ctop$indication),
         .out(read));
     AdapterFromBus#(.width(32)) wadapter_0 (.CLK(CLK), .nRST(nRST),
         .in(write),
-        .out(ctop$request));
+        .out(wadapter_0$out));
     l_top ctop (.CLK(CLK), .nRST(nRST),
-        .request(ctop$request),
-        .indication(radapter_0$in));
+        .request(wadapter_0$out),
+        .indication(ctop$indication));
 endmodule
 
 `default_nettype wire    // set back to default value
