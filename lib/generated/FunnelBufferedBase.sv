fa9da890577aa443bdf4ed388c4240335be66cd2
John Ankcorn <jca@alum.mit.edu>
2020-08-25 20:31:19 -0700

    updated lib funnelbufferedbase

--- lib/generated/FunnelBufferedBase.sv
+++ lib/generated/FunnelBufferedBase.sv
@@ -5,43 +5,25 @@ module FunnelBufferedBase #(
     parameter integer funnelWidth = 4,
     parameter integer width = 32)(
     input wire CLK, input wire nRST,
-    PipeIn.server in [funnelWidth - 1: 0],
+    PipeIn.server in[funnelWidth - 1:0],
     PipeIn.client out);
-    genvar __inst$Genvar1;
-
+    PipeIn#(.width(width)) base$in [funnelWidth - 1:0]();
     PipeIn#(.width(width)) buffer$in [funnelWidth - 1:0]();
     PipeIn#(.width(width)) buffer$out [funnelWidth - 1:0]();
+    genvar __inst$Genvar1;
     FifoPBase#(.width(width)) buffer [funnelWidth - 1:0] (.CLK(CLK), .nRST(nRST),
-        .in(in), //buffer$in),
+        .in(buffer$in),
         .out(buffer$out));
-
-    //PipeIn#(.width(width)) base$in [funnelWidth - 1:0]();
-    FunnelBase#(.funnelWidth ( funnelWidth),.width ( width)) base (.CLK(CLK), .nRST(nRST),
-        .in(buffer$out), //base$in),
+    FunnelBase#(.funnelWidth (funnelWidth),.width (width)) base (.CLK(CLK), .nRST(nRST),
+        .in(base$in),
         .out(out));
-
 for(__inst$Genvar1 = 0; __inst$Genvar1 < funnelWidth; __inst$Genvar1 = __inst$Genvar1 + 1) begin
-        //assign buffer$in[ __inst$Genvar1 ].enq__ENA = in[ __inst$Genvar1 ].enq__ENA;
-        //assign buffer$in[ __inst$Genvar1 ].enq$v = in[ __inst$Genvar1 ].enq$v;
-        //assign buffer$in[ __inst$Genvar1 ].enq__RDY = in[ __inst$Genvar1 ].enq__RDY;
-        //assign buffer$out[ __inst$Genvar1 ].enq__ENA = base$in[ __inst$Genvar1 ].enq__ENA;
-        //assign buffer$out[ __inst$Genvar1 ].enq$v = base$in[ __inst$Genvar1 ].enq$v;
-        //assign buffer$out[ __inst$Genvar1 ].enq__RDY = base$in[ __inst$Genvar1 ].enq__RDY;
-    always @( posedge CLK) begin
-      if (!nRST) begin
-      end // nRST
-      else begin
-        if (in[__inst$Genvar1].enq__ENA //&& in[__inst$Genvar1].enq__RDY
-) begin
-            $display( "funnelin %d = %x", __inst$Genvar1, in[__inst$Genvar1].enq$v);
-        end;
-        if (buffer$out[__inst$Genvar1].enq__ENA //&& buffer$out[__inst$Genvar1].enq__RDY
-) begin
-            $display( "funnelbufferout %d = %x", __inst$Genvar1, buffer$out[__inst$Genvar1].enq$v);
-        end;
-      end
-    end // always @ (posedge CLK)
-
+    assign base$in[ __inst$Genvar1 ].enq$v = buffer$out[ __inst$Genvar1 ].enq$v;
+    assign base$in[ __inst$Genvar1 ].enq__ENA = buffer$out[ __inst$Genvar1 ].enq__ENA;
+    assign buffer$in[ __inst$Genvar1 ].enq$v = in[ __inst$Genvar1 ].enq$v;
+    assign buffer$in[ __inst$Genvar1 ].enq__ENA = in[ __inst$Genvar1 ].enq__ENA;
+    assign buffer$out[ __inst$Genvar1 ].enq__RDY = base$in[ __inst$Genvar1 ].enq__RDY;
+    assign in[ __inst$Genvar1 ].enq__RDY = buffer$in[ __inst$Genvar1 ].enq__RDY;
     end;
 endmodule
 
