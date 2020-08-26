1519a029219a3e386bf7131eca5938584ba5a191
John Ankcorn <jca@alum.mit.edu>
2020-08-26 10:22:58 -0700

    update ivector

--- examples/ivector/generated/IVector.sv
+++ examples/ivector/generated/IVector.sv
@@ -2,101 +2,91 @@
 
 `default_nettype none
 module IVector (input wire CLK, input wire nRST,
-    input wire in$say__ENA,
-    input wire [32 - 1:0]in$say$meth,
-    input wire [32 - 1:0]in$say$v,
-    output wire in$say__RDY,
-    output wire out$heard__ENA,
-    output wire [32 - 1:0]out$heard$meth,
-    output wire [32 - 1:0]out$heard$v,
-    input wire out$heard__RDY);
+    IVectorRequest.server in,
+    IVectorIndication.client out);
     reg [32 - 1:0]vsize;
-    wire [32 - 1:0]RULE$respond_rule_0$temp$b;
-    wire RULE$respond_rule_0__RDY;
-    wire [32 - 1:0]RULE$respond_rule_1$temp$b;
-    wire RULE$respond_rule_1__RDY;
-    wire [32 - 1:0]RULE$respond_rule_2$temp$b;
-    wire RULE$respond_rule_2__RDY;
-    wire [32 - 1:0]RULE$respond_rule_3$temp$b;
-    wire RULE$respond_rule_3__RDY;
-    wire [32 - 1:0]RULE$respond_rule_4$temp$b;
-    wire RULE$respond_rule_4__RDY;
-    wire [32 - 1:0]RULE$respond_rule_5$temp$b;
-    wire RULE$respond_rule_5__RDY;
-    wire [32 - 1:0]RULE$respond_rule_6$temp$b;
-    wire RULE$respond_rule_6__RDY;
-    wire [32 - 1:0]RULE$respond_rule_7$temp$b;
-    wire RULE$respond_rule_7__RDY;
-    wire [32 - 1:0]RULE$respond_rule_8$temp$b;
-    wire RULE$respond_rule_8__RDY;
-    wire [32 - 1:0]RULE$respond_rule_9$temp$b;
-    wire RULE$respond_rule_9__RDY;
-    wire [((32 + 32) + 32) - 1:0]fifo$in$enq$v [10 - 1:0];
-    wire fifo$in$enq__ENA [10 - 1:0];
-    wire fifo$in$enq__RDY [10 - 1:0];
-    wire [ 10 - 1:0]fifo$in$enq__RDY_or;
-    wire fifo$in$enq__RDY_or1;
-    wire fifo$out$deq__ENA [10 - 1:0];
-    wire fifo$out$deq__RDY [10 - 1:0];
-    wire [ 10 - 1:0]fifo$out$deq__RDY_or;
-    wire fifo$out$deq__RDY_or1;
-    wire [((32 + 32) + 32) - 1:0]fifo$out$first [10 - 1:0];
-    wire fifo$out$first__RDY [10 - 1:0];
-    wire [ 10 - 1:0]fifo$out$first__RDY_or;
-    wire fifo$out$first__RDY_or1;
+    ValuePair RULE$respond_rule_0$temp;
+    ValuePair RULE$respond_rule_1$temp;
+    ValuePair RULE$respond_rule_2$temp;
+    ValuePair RULE$respond_rule_3$temp;
+    ValuePair RULE$respond_rule_4$temp;
+    ValuePair RULE$respond_rule_5$temp;
+    ValuePair RULE$respond_rule_6$temp;
+    ValuePair RULE$respond_rule_7$temp;
+    ValuePair RULE$respond_rule_8$temp;
+    ValuePair RULE$respond_rule_9$temp;
+    PipeIn#(.width(32 + 32 + 32)) fifo$in [10 - 1:0]();
+    wire [ 10 - 1:0]fifo$in__enq__RDY_or;
+    wire fifo$in__enq__RDY_or1;
+    PipeOut#(.width(32 + 32 + 32)) fifo$out [10 - 1:0]();
+    wire [ 10 - 1:0]fifo$out__deq__RDY_or;
+    wire fifo$out__deq__RDY_or1;
+    wire [ 10 - 1:0]fifo$out__first__RDY_or;
+    wire fifo$out__first__RDY_or1;
+    ValuePair in$say$agg_2e_tmp;
+    ValuePair in$say$temp;
     FifoPong fifo [10 - 1:0] (.CLK(CLK), .nRST(nRST),
-        .in$enq__ENA(fifo$in$enq__ENA),
-        .in$enq$v(fifo$in$enq$v),
-        .in$enq__RDY(fifo$in$enq__RDY),
-        .out$deq__ENA(fifo$out$deq__ENA),
-        .out$deq__RDY(fifo$out$deq__RDY),
-        .out$first(fifo$out$first),
-        .out$first__RDY(fifo$out$first__RDY));
-    assign in$say__RDY = fifo$in$enq__RDY_or1;
-    assign out$heard$meth = ( RULE$respond_rule_1__RDY ? 32'd1 : 32'd0 ) | ( RULE$respond_rule_2__RDY ? 32'd2 : 32'd0 ) | ( RULE$respond_rule_3__RDY ? 32'd3 : 32'd0 ) | ( RULE$respond_rule_4__RDY ? 32'd4 : 32'd0 ) | ( RULE$respond_rule_5__RDY ? 32'd5 : 32'd0 ) | ( RULE$respond_rule_6__RDY ? 32'd6 : 32'd0 ) | ( RULE$respond_rule_7__RDY ? 32'd7 : 32'd0 ) | ( RULE$respond_rule_8__RDY ? 32'd8 : 32'd0 ) | ( RULE$respond_rule_9__RDY ? 32'd9 : 32'd0 );
-    assign out$heard$v = ( RULE$respond_rule_0__RDY ? RULE$respond_rule_0$temp$b : 32'd0 ) | ( RULE$respond_rule_1__RDY ? RULE$respond_rule_1$temp$b : 32'd0 ) | ( RULE$respond_rule_2__RDY ? RULE$respond_rule_2$temp$b : 32'd0 ) | ( RULE$respond_rule_3__RDY ? RULE$respond_rule_3$temp$b : 32'd0 ) | ( RULE$respond_rule_4__RDY ? RULE$respond_rule_4$temp$b : 32'd0 ) | ( RULE$respond_rule_5__RDY ? RULE$respond_rule_5$temp$b : 32'd0 ) | ( RULE$respond_rule_6__RDY ? RULE$respond_rule_6$temp$b : 32'd0 ) | ( RULE$respond_rule_7__RDY ? RULE$respond_rule_7$temp$b : 32'd0 ) | ( RULE$respond_rule_8__RDY ? RULE$respond_rule_8$temp$b : 32'd0 ) | ( RULE$respond_rule_9__RDY ? RULE$respond_rule_9$temp$b : 32'd0 );
-    assign out$heard__ENA = fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1;
+        .in(fifo$in),
+        .out(fifo$out));
     // Extra assigments, not to output wires
-    assign RULE$respond_rule_0$temp$b = RULE$respond_rule_0$temp$lettemp[ 32 - 1 + 32 : 32 ];
-    assign RULE$respond_rule_0$temp$lettemp = fifo[ 0 ]  out$first;
-    assign RULE$respond_rule_0__RDY = fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-    assign RULE$respond_rule_1$temp$b = RULE$respond_rule_1$temp$lettemp[ 32 - 1 + 32 : 32 ];
-    assign RULE$respond_rule_1$temp$lettemp = fifo[ 1 ]  out$first;
-    assign RULE$respond_rule_1__RDY = fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-    assign RULE$respond_rule_2$temp$b = RULE$respond_rule_2$temp$lettemp[ 32 - 1 + 32 : 32 ];
-    assign RULE$respond_rule_2$temp$lettemp = fifo[ 2 ]  out$first;
-    assign RULE$respond_rule_2__RDY = fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-    assign RULE$respond_rule_3$temp$b = RULE$respond_rule_3$temp$lettemp[ 32 - 1 + 32 : 32 ];
-    assign RULE$respond_rule_3$temp$lettemp = fifo[ 3 ]  out$first;
-    assign RULE$respond_rule_3__RDY = fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-    assign RULE$respond_rule_4$temp$b = RULE$respond_rule_4$temp$lettemp[ 32 - 1 + 32 : 32 ];
-    assign RULE$respond_rule_4$temp$lettemp = fifo[ 4 ]  out$first;
-    assign RULE$respond_rule_4__RDY = fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-    assign RULE$respond_rule_5$temp$b = RULE$respond_rule_5$temp$lettemp[ 32 - 1 + 32 : 32 ];
-    assign RULE$respond_rule_5$temp$lettemp = fifo[ 5 ]  out$first;
-    assign RULE$respond_rule_5__RDY = fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-    assign RULE$respond_rule_6$temp$b = RULE$respond_rule_6$temp$lettemp[ 32 - 1 + 32 : 32 ];
-    assign RULE$respond_rule_6$temp$lettemp = fifo[ 6 ]  out$first;
-    assign RULE$respond_rule_6__RDY = fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-    assign RULE$respond_rule_7$temp$b = RULE$respond_rule_7$temp$lettemp[ 32 - 1 + 32 : 32 ];
-    assign RULE$respond_rule_7$temp$lettemp = fifo[ 7 ]  out$first;
-    assign RULE$respond_rule_7__RDY = fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-    assign RULE$respond_rule_8$temp$b = RULE$respond_rule_8$temp$lettemp[ 32 - 1 + 32 : 32 ];
-    assign RULE$respond_rule_8$temp$lettemp = fifo[ 8 ]  out$first;
-    assign RULE$respond_rule_8__RDY = fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-    assign RULE$respond_rule_9$temp$b = RULE$respond_rule_9$temp$lettemp[ 32 - 1 + 32 : 32 ];
-    assign RULE$respond_rule_9$temp$lettemp = fifo[ 9 ]  out$first;
-    assign RULE$respond_rule_9__RDY = fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-    assign fifo$in$enq__RDY_or1 = |fifo$in$enq__RDY_or;
-    assign fifo$out$deq__RDY_or1 = |fifo$out$deq__RDY_or;
-    assign fifo$out$first__RDY_or1 = |fifo$out$first__RDY_or;
+    assign RULE$respond_rule_0$temp.a = RULE$respond_rule_0$temp.lettemp[ 32 - 1 : 0 ];
+    assign RULE$respond_rule_0$temp.b = RULE$respond_rule_0$temp.lettemp[ 32 - 1 + 32 : 32 ];
+    assign RULE$respond_rule_0$temp.c = RULE$respond_rule_0$temp.lettemp[ 32 - 1 + 64 : 64 ];
+    assign RULE$respond_rule_0$temp.lettemp = fifo[ 0 ]  out.first;
+    assign RULE$respond_rule_1$temp.a = RULE$respond_rule_1$temp.lettemp[ 32 - 1 : 0 ];
+    assign RULE$respond_rule_1$temp.b = RULE$respond_rule_1$temp.lettemp[ 32 - 1 + 32 : 32 ];
+    assign RULE$respond_rule_1$temp.c = RULE$respond_rule_1$temp.lettemp[ 32 - 1 + 64 : 64 ];
+    assign RULE$respond_rule_1$temp.lettemp = fifo[ 1 ]  out.first;
+    assign RULE$respond_rule_2$temp.a = RULE$respond_rule_2$temp.lettemp[ 32 - 1 : 0 ];
+    assign RULE$respond_rule_2$temp.b = RULE$respond_rule_2$temp.lettemp[ 32 - 1 + 32 : 32 ];
+    assign RULE$respond_rule_2$temp.c = RULE$respond_rule_2$temp.lettemp[ 32 - 1 + 64 : 64 ];
+    assign RULE$respond_rule_2$temp.lettemp = fifo[ 2 ]  out.first;
+    assign RULE$respond_rule_3$temp.a = RULE$respond_rule_3$temp.lettemp[ 32 - 1 : 0 ];
+    assign RULE$respond_rule_3$temp.b = RULE$respond_rule_3$temp.lettemp[ 32 - 1 + 32 : 32 ];
+    assign RULE$respond_rule_3$temp.c = RULE$respond_rule_3$temp.lettemp[ 32 - 1 + 64 : 64 ];
+    assign RULE$respond_rule_3$temp.lettemp = fifo[ 3 ]  out.first;
+    assign RULE$respond_rule_4$temp.a = RULE$respond_rule_4$temp.lettemp[ 32 - 1 : 0 ];
+    assign RULE$respond_rule_4$temp.b = RULE$respond_rule_4$temp.lettemp[ 32 - 1 + 32 : 32 ];
+    assign RULE$respond_rule_4$temp.c = RULE$respond_rule_4$temp.lettemp[ 32 - 1 + 64 : 64 ];
+    assign RULE$respond_rule_4$temp.lettemp = fifo[ 4 ]  out.first;
+    assign RULE$respond_rule_5$temp.a = RULE$respond_rule_5$temp.lettemp[ 32 - 1 : 0 ];
+    assign RULE$respond_rule_5$temp.b = RULE$respond_rule_5$temp.lettemp[ 32 - 1 + 32 : 32 ];
+    assign RULE$respond_rule_5$temp.c = RULE$respond_rule_5$temp.lettemp[ 32 - 1 + 64 : 64 ];
+    assign RULE$respond_rule_5$temp.lettemp = fifo[ 5 ]  out.first;
+    assign RULE$respond_rule_6$temp.a = RULE$respond_rule_6$temp.lettemp[ 32 - 1 : 0 ];
+    assign RULE$respond_rule_6$temp.b = RULE$respond_rule_6$temp.lettemp[ 32 - 1 + 32 : 32 ];
+    assign RULE$respond_rule_6$temp.c = RULE$respond_rule_6$temp.lettemp[ 32 - 1 + 64 : 64 ];
+    assign RULE$respond_rule_6$temp.lettemp = fifo[ 6 ]  out.first;
+    assign RULE$respond_rule_7$temp.a = RULE$respond_rule_7$temp.lettemp[ 32 - 1 : 0 ];
+    assign RULE$respond_rule_7$temp.b = RULE$respond_rule_7$temp.lettemp[ 32 - 1 + 32 : 32 ];
+    assign RULE$respond_rule_7$temp.c = RULE$respond_rule_7$temp.lettemp[ 32 - 1 + 64 : 64 ];
+    assign RULE$respond_rule_7$temp.lettemp = fifo[ 7 ]  out.first;
+    assign RULE$respond_rule_8$temp.a = RULE$respond_rule_8$temp.lettemp[ 32 - 1 : 0 ];
+    assign RULE$respond_rule_8$temp.b = RULE$respond_rule_8$temp.lettemp[ 32 - 1 + 32 : 32 ];
+    assign RULE$respond_rule_8$temp.c = RULE$respond_rule_8$temp.lettemp[ 32 - 1 + 64 : 64 ];
+    assign RULE$respond_rule_8$temp.lettemp = fifo[ 8 ]  out.first;
+    assign RULE$respond_rule_9$temp.a = RULE$respond_rule_9$temp.lettemp[ 32 - 1 : 0 ];
+    assign RULE$respond_rule_9$temp.b = RULE$respond_rule_9$temp.lettemp[ 32 - 1 + 32 : 32 ];
+    assign RULE$respond_rule_9$temp.c = RULE$respond_rule_9$temp.lettemp[ 32 - 1 + 64 : 64 ];
+    assign RULE$respond_rule_9$temp.lettemp = fifo[ 9 ]  out.first;
+    assign fifo$in__enq__RDY_or1 = |fifo$in__enq__RDY_or;
+    assign fifo$out__deq__RDY_or1 = |fifo$out__deq__RDY_or;
+    assign fifo$out__first__RDY_or1 = |fifo$out__first__RDY_or;
+    assign in$say$temp.b = in.say$v;
+    assign in.say$agg_2e_tmp.a = in.say$temp[ 32 - 1 : 0 ];
+    assign in.say$agg_2e_tmp.b = in.say$temp[ 32 - 1 + 32 : 32 ];
+    assign in.say$agg_2e_tmp.c = in.say$temp[ 32 - 1 + 64 : 64 ];
+    assign in.say__RDY = fifo$in__enq__RDY_or1;
+    assign out.heard$meth = ( RULE$respond_rule_1__RDY ? 1 : 32'd0 ) | ( RULE$respond_rule_2__RDY ? 2 : 32'd0 ) | ( RULE$respond_rule_3__RDY ? 3 : 32'd0 ) | ( RULE$respond_rule_4__RDY ? 4 : 32'd0 ) | ( RULE$respond_rule_5__RDY ? 5 : 32'd0 ) | ( RULE$respond_rule_6__RDY ? 6 : 32'd0 ) | ( RULE$respond_rule_7__RDY ? 7 : 32'd0 ) | ( RULE$respond_rule_8__RDY ? 8 : 32'd0 ) | ( RULE$respond_rule_9__RDY ? 9 : 32'd0 );
+    assign out.heard$v = ( RULE$respond_rule_0__RDY ? RULE$respond_rule_0$temp.b : 32'd0 ) | ( RULE$respond_rule_1__RDY ? RULE$respond_rule_1$temp.b : 32'd0 ) | ( RULE$respond_rule_2__RDY ? RULE$respond_rule_2$temp.b : 32'd0 ) | ( RULE$respond_rule_3__RDY ? RULE$respond_rule_3$temp.b : 32'd0 ) | ( RULE$respond_rule_4__RDY ? RULE$respond_rule_4$temp.b : 32'd0 ) | ( RULE$respond_rule_5__RDY ? RULE$respond_rule_5$temp.b : 32'd0 ) | ( RULE$respond_rule_6__RDY ? RULE$respond_rule_6$temp.b : 32'd0 ) | ( RULE$respond_rule_7__RDY ? RULE$respond_rule_7$temp.b : 32'd0 ) | ( RULE$respond_rule_8__RDY ? RULE$respond_rule_8$temp.b : 32'd0 ) | ( RULE$respond_rule_9__RDY ? RULE$respond_rule_9$temp.b : 32'd0 );
+    assign out.heard__ENA = fifo$out__first__RDY_or1 && fifo$out__deq__RDY_or1;
 for(__inst$Genvar1 = 0; __inst$Genvar1 < 10; __inst$Genvar1 = __inst$Genvar1 + 1) begin
-        assign fifo$in$enq$v[__inst$Genvar1] = in$say$agg_2e_tmp;
-        assign fifo$in$enq__ENA[__inst$Genvar1] = in$say__ENA && ( in$say$meth == __inst$Genvar1 );
-        assign fifo$in$enq__RDY_or[__inst$Genvar1] = fifo$in$enq__RDY[ __inst$Genvar1 ];
-        assign fifo$out$deq__ENA[__inst$Genvar1] = fifo$out$first__RDY_or1 && out$heard__RDY && ( ( 0 == __inst$Genvar1 ) || ( 1 == __inst$Genvar1 ) || ( 2 == __inst$Genvar1 ) || ( 3 == __inst$Genvar1 ) || ( 4 == __inst$Genvar1 ) || ( 5 == __inst$Genvar1 ) || ( 6 == __inst$Genvar1 ) || ( 7 == __inst$Genvar1 ) || ( 8 == __inst$Genvar1 ) || ( 9 == __inst$Genvar1 ) );
-        assign fifo$out$deq__RDY_or[__inst$Genvar1] = fifo$out$deq__RDY[ __inst$Genvar1 ];
-        assign fifo$out$first__RDY_or[__inst$Genvar1] = fifo$out$first__RDY[ __inst$Genvar1 ];
+    assign fifo$in[__inst$Genvar1].enq$v = in.say$agg_2e_tmp;
+    assign fifo$in[__inst$Genvar1].enq__ENA = in.say__ENA && fifo$in__enq__RDY_or1 && ( in.say$meth == __inst$Genvar1 );
+    assign fifo$in__enq__RDY_or[__inst$Genvar1] = fifo$in[ __inst$Genvar1 ].enq__RDY;
+    assign fifo$out[__inst$Genvar1].deq__ENA = fifo$out__first__RDY_or1 && fifo$out__deq__RDY_or1 && out.heard__RDY && ( ( 0 == __inst$Genvar1 ) || ( 1 == __inst$Genvar1 ) || ( 2 == __inst$Genvar1 ) || ( 3 == __inst$Genvar1 ) || ( 4 == __inst$Genvar1 ) || ( 5 == __inst$Genvar1 ) || ( 6 == __inst$Genvar1 ) || ( 7 == __inst$Genvar1 ) || ( 8 == __inst$Genvar1 ) || ( 9 == __inst$Genvar1 ) );
+    assign fifo$out__deq__RDY_or[__inst$Genvar1] = fifo$out[ __inst$Genvar1 ].deq__RDY;
+    assign fifo$out__first__RDY_or[__inst$Genvar1] = fifo$out[ __inst$Genvar1 ].first__RDY;
     end;
 
     always @( posedge CLK) begin
--- examples/ivector/generated/ivector.generated.vh
+++ examples/ivector/generated/ivector.generated.vh
@@ -2,101 +2,6 @@
 `define __ivector_GENERATED__VH__
 `include "atomicclib.vh"
 
-`ifndef __IVectorTest_DEF__
-`define __IVectorTest_DEF__
-typedef struct packed {
-    logic [((1 * 10) + 32) - 1:0] ivector;
-} IVectorTest;
-`endif
-`ifndef __NOCDataH_DEF__
-`define __NOCDataH_DEF__
-typedef struct packed {
-    logic [128 - 1:0] data;
-    logic [16 - 1:0] length;
-} NOCDataH;
-`endif
-`ifndef __ValuePair_DEF__
-`define __ValuePair_DEF__
-typedef struct packed {
-    logic [32 - 1:0] c;
-    logic [32 - 1:0] b;
-    logic [32 - 1:0] a;
-} ValuePair;
-`endif
-`ifndef __PipeIn_OC_4_DEF__
-`define __PipeIn_OC_4_DEF__
-interface PipeIn_OC_4#(width = 96);
-    logic enq__ENA;
-    logic [width - 1:0] enq$v;
-    logic enq__RDY;
-    modport server (input  enq__ENA, enq$v,
-                    output enq__RDY);
-    modport client (output enq__ENA, enq$v,
-                    input  enq__RDY);
-endinterface
-`endif
-`ifndef __PipeOut_OC_5_DEF__
-`define __PipeOut_OC_5_DEF__
-interface PipeOut_OC_5#(width = 96);
-    logic deq__ENA;
-    logic deq__RDY;
-    logic first;
-    logic first__RDY;
-    modport server (input  deq__ENA, first,
-                    output deq__RDY, first__RDY);
-    modport client (output deq__ENA, first,
-                    input  deq__RDY, first__RDY);
-endinterface
-`endif
-`ifndef __PipeIn_OC_2_DEF__
-`define __PipeIn_OC_2_DEF__
-interface PipeIn_OC_2;
-    logic enq__ENA;
-    logic [((32 + 32) + 32) - 1:0] enq$v;
-    logic enq__RDY;
-    modport server (input  enq__ENA, enq$v,
-                    output enq__RDY);
-    modport client (output enq__ENA, enq$v,
-                    input  enq__RDY);
-endinterface
-`endif
-`ifndef __PipeOut_DEF__
-`define __PipeOut_DEF__
-interface PipeOut;
-    logic deq__ENA;
-    logic deq__RDY;
-    logic first;
-    logic first__RDY;
-    modport server (input  deq__ENA, first,
-                    output deq__RDY, first__RDY);
-    modport client (output deq__ENA, first,
-                    input  deq__RDY, first__RDY);
-endinterface
-`endif
-`ifndef __PipeIn_OC_0_DEF__
-`define __PipeIn_OC_0_DEF__
-interface PipeIn_OC_0#(dataWidth = 32, funnelWidth = 99);
-    logic enq__ENA;
-    logic [dataWidth - 1:0] enq$v;
-    logic enq__RDY;
-    modport server (input  enq__ENA, enq$v,
-                    output enq__RDY);
-    modport client (output enq__ENA, enq$v,
-                    input  enq__RDY);
-endinterface
-`endif
-`ifndef __PipeIn_OC_1_DEF__
-`define __PipeIn_OC_1_DEF__
-interface PipeIn_OC_1#(dataWidth = 32, funnelWidth = 99);
-    logic enq__ENA;
-    logic [dataWidth - 1:0] enq$v;
-    logic enq__RDY;
-    modport server (input  enq__ENA, enq$v,
-                    output enq__RDY);
-    modport client (output enq__ENA, enq$v,
-                    input  enq__RDY);
-endinterface
-`endif
 `ifndef __IVectorRequest_DEF__
 `define __IVectorRequest_DEF__
 interface IVectorRequest;
@@ -123,27 +28,14 @@ interface IVectorIndication;
                     input  heard__RDY);
 endinterface
 `endif
-`ifndef __PipeIn_DEF__
-`define __PipeIn_DEF__
-interface PipeIn;
-    logic enq__ENA;
-    logic [(16 + 128) - 1:0] enq$v;
-    logic enq__RDY;
-    modport server (input  enq__ENA, enq$v,
-                    output enq__RDY);
-    modport client (output enq__ENA, enq$v,
-                    input  enq__RDY);
-endinterface
-`endif
 //METASTART; FifoPong
 //METAINTERNAL; element1; Fifo1Base(width=96);
 //METAINTERNAL; element2; Fifo1Base(width=96);
-//METAINVOKE; in$enq__ENA; !pong:element1$in$enq__ENA;pong:element2$in$enq__ENA;
-//METAGUARD; in$enq; ( element2$in$enq__RDY && ( pong || element1$in$enq__RDY ) ) || ( ( !element2$in$enq__RDY ) && ( !( pong || ( !element1$in$enq__RDY ) ) ) );
-//METAINVOKE; out$deq__ENA; !pong:element1$out$deq__ENA;pong:element2$out$deq__ENA;
-//METAGUARD; out$deq; ( element2$out$deq__RDY && ( pong || element1$out$deq__RDY ) ) || ( ( !element2$out$deq__RDY ) && ( !( pong || ( !element1$out$deq__RDY ) ) ) );
-//METAINVOKE; out$first; !pong:element1$out$first;pong:element2$out$first;
-//METAGUARD; out$first; ( element2$out$first__RDY && ( pong || element1$out$first__RDY ) ) || ( ( !element2$out$first__RDY ) && ( !( pong || ( !element1$out$first__RDY ) ) ) );
+//METAINVOKE; in.enq__ENA; !pong:element1$in.enq__ENA;pong:element2$in.enq__ENA;
+//METAGUARD; in.enq; ( element2$in.enq__RDY && ( pong || element1$in.enq__RDY ) ) || ( ( !element2$in.enq__RDY ) && ( !( pong || ( !element1$in.enq__RDY ) ) ) );
+//METAINVOKE; out.deq__ENA; !pong:element1$out.deq__ENA;pong:element2$out.deq__ENA;
+//METAGUARD; out.deq; ( element2$out.deq__RDY && ( pong || element1$out.deq__RDY ) ) || ( ( !element2$out.deq__RDY ) && ( !( pong || ( !element1$out.deq__RDY ) ) ) );
+//METAGUARD; out.first; ( element2$out.first__RDY && ( pong || element1$out.first__RDY ) ) || ( ( !element2$out.first__RDY ) && ( !( pong || ( !element1$out.first__RDY ) ) ) );
 //METASTART; IVector
 //METAINTERNAL; fifo0; FifoPong;
 //METAINTERNAL; fifo1; FifoPong;
@@ -155,36 +47,36 @@ endinterface
 //METAINTERNAL; fifo7; FifoPong;
 //METAINTERNAL; fifo8; FifoPong;
 //METAINTERNAL; fifo9; FifoPong;
-//METAINVOKE; in$say__ENA; :fifo$in$enq__ENA;
-//METAGUARD; in$say; fifo$in$enq__RDY_or1;
-//METAINVOKE; RULE$respond_rule_0__ENA; :fifo$out$deq__ENA;:fifo$out$first;:out$heard__ENA;
+//METAINVOKE; in.say__ENA; :fifo$in[__inst$Genvar1].enq__ENA;
+//METAGUARD; in.say; fifo$in__enq__RDY_or1;
+//METAINVOKE; RULE$respond_rule_0__ENA; :fifo$out[__inst$Genvar1].deq__ENA;:out.heard__ENA;
 //METAEXCLUSIVE; RULE$respond_rule_0__ENA; RULE$respond_rule_1__ENA; RULE$respond_rule_2__ENA; RULE$respond_rule_3__ENA; RULE$respond_rule_4__ENA; RULE$respond_rule_5__ENA; RULE$respond_rule_6__ENA; RULE$respond_rule_7__ENA; RULE$respond_rule_8__ENA; RULE$respond_rule_9__ENA
-//METAGUARD; RULE$respond_rule_0; fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-//METAINVOKE; RULE$respond_rule_1__ENA; :fifo$out$deq__ENA;:fifo$out$first;:out$heard__ENA;
+//METAGUARD; RULE$respond_rule_0; fifo$out__first__RDY_or1 && fifo$out__deq__RDY_or1 && out.heard__RDY;
+//METAINVOKE; RULE$respond_rule_1__ENA; :fifo$out[__inst$Genvar1].deq__ENA;:out.heard__ENA;
 //METAEXCLUSIVE; RULE$respond_rule_1__ENA; RULE$respond_rule_2__ENA; RULE$respond_rule_3__ENA; RULE$respond_rule_4__ENA; RULE$respond_rule_5__ENA; RULE$respond_rule_6__ENA; RULE$respond_rule_7__ENA; RULE$respond_rule_8__ENA; RULE$respond_rule_9__ENA
-//METAGUARD; RULE$respond_rule_1; fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-//METAINVOKE; RULE$respond_rule_2__ENA; :fifo$out$deq__ENA;:fifo$out$first;:out$heard__ENA;
+//METAGUARD; RULE$respond_rule_1; fifo$out__first__RDY_or1 && fifo$out__deq__RDY_or1 && out.heard__RDY;
+//METAINVOKE; RULE$respond_rule_2__ENA; :fifo$out[__inst$Genvar1].deq__ENA;:out.heard__ENA;
 //METAEXCLUSIVE; RULE$respond_rule_2__ENA; RULE$respond_rule_3__ENA; RULE$respond_rule_4__ENA; RULE$respond_rule_5__ENA; RULE$respond_rule_6__ENA; RULE$respond_rule_7__ENA; RULE$respond_rule_8__ENA; RULE$respond_rule_9__ENA
-//METAGUARD; RULE$respond_rule_2; fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-//METAINVOKE; RULE$respond_rule_3__ENA; :fifo$out$deq__ENA;:fifo$out$first;:out$heard__ENA;
+//METAGUARD; RULE$respond_rule_2; fifo$out__first__RDY_or1 && fifo$out__deq__RDY_or1 && out.heard__RDY;
+//METAINVOKE; RULE$respond_rule_3__ENA; :fifo$out[__inst$Genvar1].deq__ENA;:out.heard__ENA;
 //METAEXCLUSIVE; RULE$respond_rule_3__ENA; RULE$respond_rule_4__ENA; RULE$respond_rule_5__ENA; RULE$respond_rule_6__ENA; RULE$respond_rule_7__ENA; RULE$respond_rule_8__ENA; RULE$respond_rule_9__ENA
-//METAGUARD; RULE$respond_rule_3; fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-//METAINVOKE; RULE$respond_rule_4__ENA; :fifo$out$deq__ENA;:fifo$out$first;:out$heard__ENA;
+//METAGUARD; RULE$respond_rule_3; fifo$out__first__RDY_or1 && fifo$out__deq__RDY_or1 && out.heard__RDY;
+//METAINVOKE; RULE$respond_rule_4__ENA; :fifo$out[__inst$Genvar1].deq__ENA;:out.heard__ENA;
 //METAEXCLUSIVE; RULE$respond_rule_4__ENA; RULE$respond_rule_5__ENA; RULE$respond_rule_6__ENA; RULE$respond_rule_7__ENA; RULE$respond_rule_8__ENA; RULE$respond_rule_9__ENA
-//METAGUARD; RULE$respond_rule_4; fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-//METAINVOKE; RULE$respond_rule_5__ENA; :fifo$out$deq__ENA;:fifo$out$first;:out$heard__ENA;
+//METAGUARD; RULE$respond_rule_4; fifo$out__first__RDY_or1 && fifo$out__deq__RDY_or1 && out.heard__RDY;
+//METAINVOKE; RULE$respond_rule_5__ENA; :fifo$out[__inst$Genvar1].deq__ENA;:out.heard__ENA;
 //METAEXCLUSIVE; RULE$respond_rule_5__ENA; RULE$respond_rule_6__ENA; RULE$respond_rule_7__ENA; RULE$respond_rule_8__ENA; RULE$respond_rule_9__ENA
-//METAGUARD; RULE$respond_rule_5; fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-//METAINVOKE; RULE$respond_rule_6__ENA; :fifo$out$deq__ENA;:fifo$out$first;:out$heard__ENA;
+//METAGUARD; RULE$respond_rule_5; fifo$out__first__RDY_or1 && fifo$out__deq__RDY_or1 && out.heard__RDY;
+//METAINVOKE; RULE$respond_rule_6__ENA; :fifo$out[__inst$Genvar1].deq__ENA;:out.heard__ENA;
 //METAEXCLUSIVE; RULE$respond_rule_6__ENA; RULE$respond_rule_7__ENA; RULE$respond_rule_8__ENA; RULE$respond_rule_9__ENA
-//METAGUARD; RULE$respond_rule_6; fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-//METAINVOKE; RULE$respond_rule_7__ENA; :fifo$out$deq__ENA;:fifo$out$first;:out$heard__ENA;
+//METAGUARD; RULE$respond_rule_6; fifo$out__first__RDY_or1 && fifo$out__deq__RDY_or1 && out.heard__RDY;
+//METAINVOKE; RULE$respond_rule_7__ENA; :fifo$out[__inst$Genvar1].deq__ENA;:out.heard__ENA;
 //METAEXCLUSIVE; RULE$respond_rule_7__ENA; RULE$respond_rule_8__ENA; RULE$respond_rule_9__ENA
-//METAGUARD; RULE$respond_rule_7; fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-//METAINVOKE; RULE$respond_rule_8__ENA; :fifo$out$deq__ENA;:fifo$out$first;:out$heard__ENA;
+//METAGUARD; RULE$respond_rule_7; fifo$out__first__RDY_or1 && fifo$out__deq__RDY_or1 && out.heard__RDY;
+//METAINVOKE; RULE$respond_rule_8__ENA; :fifo$out[__inst$Genvar1].deq__ENA;:out.heard__ENA;
 //METAEXCLUSIVE; RULE$respond_rule_8__ENA; RULE$respond_rule_9__ENA
-//METAGUARD; RULE$respond_rule_8; fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
-//METAINVOKE; RULE$respond_rule_9__ENA; :fifo$out$deq__ENA;:fifo$out$first;:out$heard__ENA;
-//METAGUARD; RULE$respond_rule_9; fifo$out$first__RDY_or1 && fifo$out$deq__RDY_or1 && out$heard__RDY;
+//METAGUARD; RULE$respond_rule_8; fifo$out__first__RDY_or1 && fifo$out__deq__RDY_or1 && out.heard__RDY;
+//METAINVOKE; RULE$respond_rule_9__ENA; :fifo$out[__inst$Genvar1].deq__ENA;:out.heard__ENA;
+//METAGUARD; RULE$respond_rule_9; fifo$out__first__RDY_or1 && fifo$out__deq__RDY_or1 && out.heard__RDY;
 //METARULES; RULE$respond_rule_0; RULE$respond_rule_1; RULE$respond_rule_2; RULE$respond_rule_3; RULE$respond_rule_4; RULE$respond_rule_5; RULE$respond_rule_6; RULE$respond_rule_7; RULE$respond_rule_8; RULE$respond_rule_9
 `endif
