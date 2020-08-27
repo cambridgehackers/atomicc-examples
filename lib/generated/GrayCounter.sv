1e70bdbfb9368b266cedefc55edd475874321dfa
John Ankcorn <jca@alum.mit.edu>
2020-08-27 16:26:14 -0700

    update graycounter

--- lib/generated/GrayCounter.sv
+++ lib/generated/GrayCounter.sv
@@ -20,7 +20,6 @@ module GrayCounter #(
     output wire writeBin__RDY);
     reg counter [width - 1:0];
     wire RULE$incdec$useLsb;
-    wire RULE$incdec__RDY;
     wire [width - 1:0]counterBit;
     wire [width - 1:0]m;
     wire [width - 1:0]readBin$rtemp;
@@ -36,7 +35,6 @@ module GrayCounter #(
     assign writeGray__RDY = 1;
     // Extra assigments, not to output wires
     assign RULE$incdec$useLsb = ( ^counterBit ) == decrement__ENA;
-    assign RULE$incdec__RDY = !( increment__ENA == decrement__ENA );
 for(__inst$Genvar1 = 0; __inst$Genvar1 < width; __inst$Genvar1 = __inst$Genvar1 + 1) begin
     assign counterBit[ __inst$Genvar1 ] = counter[ __inst$Genvar1 ];
     assign readBin$rtemp[ __inst$Genvar1 ] = readBin$temp[ __inst$Genvar1 ];
@@ -49,14 +47,14 @@ for(__inst$Genvar1 = 0; __inst$Genvar1 < width; __inst$Genvar1 = __inst$Genvar1 
       if (!nRST) begin
       end // nRST
       else begin
-        if (RULE$incdec__RDY) begin // RULE$incdec__ENA
+        if (!( increment__ENA == decrement__ENA )) begin // RULE$incdec__ENA
             counter[ __inst$Genvar1 ] <= counter[ __inst$Genvar1 ] ^ ( ( __inst$Genvar1 == 0 ) ? RULE$incdec$useLsb : ( ( ( RULE$incdec$useLsb != 0 ) ^ 1 ) & ( ( __inst$Genvar1 == ( width - 1 ) ) | counterBit[ ( ( ( !( ( __inst$Genvar1 < 1 ) || ( __inst$Genvar1 == 0 ) ) ) ? ( __inst$Genvar1 - 1 ) : 0 ) ) ] ) & ( ( __inst$Genvar1 == 1 ) | ( ( ( |counterBit[ ( ( ( !( ( __inst$Genvar1 < 2 ) || ( __inst$Genvar1 == 0 ) ) ) ? ( __inst$Genvar1 - 2 ) : 0 ) ) : 0 ] ) != 0 ) ^ 1 ) ) ) );
         end; // End of RULE$incdec__ENA
         if (writeBin__ENA) begin // writeBin__ENA
-            if (__inst$Genvar1 == ( width - 1 ))
-            counter[ __inst$Genvar1 ] <= writeBin$v[ __inst$Genvar1 ];
             if (!( __inst$Genvar1 == ( width - 1 ) ))
             counter[ __inst$Genvar1 ] <= ^writeBin$v[ ( __inst$Genvar1 + 1 ) : __inst$Genvar1 ];
+            if (__inst$Genvar1 == ( width - 1 ))
+            counter[ __inst$Genvar1 ] <= writeBin$v[ __inst$Genvar1 ];
         end; // End of writeBin__ENA
         if (writeGray__ENA) begin // writeGray__ENA
             counter[ __inst$Genvar1 ] <= writeGray$v[ __inst$Genvar1 ];
