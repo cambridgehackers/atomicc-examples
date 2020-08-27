becdee1ed40aca22bd3c5e46342c7dda6b6c8242
John Ankcorn <jca@alum.mit.edu>
2020-08-27 12:09:47 -0700

    updated rulec

--- examples/rulec/generated/Echo.sv
+++ examples/rulec/generated/Echo.sv
@@ -13,16 +13,20 @@ module Echo (input wire CLK, input wire nRST,
     reg [32 - 1:0]v_delay;
     reg [32 - 1:0]v_temp;
     reg [32 - 1:0]v_type;
+    wire RULE$delay_rule__ENA;
     wire RULE$delay_rule__RDY;
+    wire RULE$respond_rule__ENA;
     wire RULE$respond_rule__RDY;
     // Extra assigments, not to output wires
+    assign RULE$delay_rule__ENA = !( ( ( busy != 0 ) & ( busy_delay == 0 ) ) == 0 );
     assign RULE$delay_rule__RDY = !( ( ( busy != 0 ) & ( busy_delay == 0 ) ) == 0 );
+    assign RULE$respond_rule__ENA = !( ( busy_delay == 0 ) || ( !( ( indication.heard__RDY && ( ( v_type == 1 ) || indication.heard2__RDY ) ) || ( ( !indication.heard__RDY ) && ( !( ( v_type == 1 ) || ( !indication.heard2__RDY ) ) ) ) ) ) );
     assign RULE$respond_rule__RDY = !( ( busy_delay == 0 ) || ( !( ( indication.heard__RDY && ( ( v_type == 1 ) || indication.heard2__RDY ) ) || ( ( !indication.heard__RDY ) && ( !( ( v_type == 1 ) || ( !indication.heard2__RDY ) ) ) ) ) ) );
     assign indication.heard$v = v_delay;
     assign indication.heard2$a = a_delay;
     assign indication.heard2$b = b_delay;
-    assign indication.heard2__ENA = !( ( v_type == 1 ) || ( !RULE$respond_rule__RDY ) );
-    assign indication.heard__ENA = RULE$respond_rule__RDY && ( v_type == 1 );
+    assign indication.heard2__ENA = !( ( v_type == 1 ) || ( busy_delay == 0 ) );
+    assign indication.heard__ENA = !( ( busy_delay == 0 ) || ( !( v_type == 1 ) ) );
     assign request.say2__RDY = !( 0 == ( ( busy != 0 ) ^ 1 ) );
     assign request.say__RDY = !( 0 == ( ( busy != 0 ) ^ 1 ) );
     assign request.setLeds__RDY = 1;
@@ -40,14 +44,14 @@ module Echo (input wire CLK, input wire nRST,
         v_type <= 0;
       end // nRST
       else begin
-        if (RULE$delay_rule__RDY) begin // RULE$delay_rule__ENA
+        if (RULE$delay_rule__ENA && RULE$delay_rule__RDY) begin // RULE$delay_rule__ENA
             busy <= 0;
             busy_delay <= 1;
             v_delay <= v_temp;
             a_delay <= a_temp;
             b_delay <= b_temp;
         end; // End of RULE$delay_rule__ENA
-        if (RULE$respond_rule__RDY) begin // RULE$respond_rule__ENA
+        if (RULE$respond_rule__ENA && RULE$respond_rule__RDY) begin // RULE$respond_rule__ENA
             busy_delay <= 0;
         end; // End of RULE$respond_rule__ENA
         if (request.say2__ENA && request.say2__RDY) begin // request.say2__ENA
