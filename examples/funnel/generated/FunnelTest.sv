3b931bb0642714cbaf7dbc4f1ed0a797c3a901e9
John Ankcorn <jca@alum.mit.edu>
2020-08-27 22:30:49 -0700

    update funneltest

--- examples/funnel/generated/FunnelTest.sv
+++ examples/funnel/generated/FunnelTest.sv
@@ -5,6 +5,7 @@ module FunnelTest (input wire CLK, input wire nRST,
     FunnelRequest.server request,
     FunnelIndication.client indication);
     reg [8 - 1:0]index;
+    wire RULE$respond_rule__RDY;
     PipeIn#(.width(32)) fifo$in [4 - 1:0]();
     wire [ 4 - 1:0]fifo$in__enq__RDY_or;
     wire fifo$in__enq__RDY_or1;
@@ -23,6 +24,7 @@ module FunnelTest (input wire CLK, input wire nRST,
         .in(funnel$out),
         .out(result$out));
     // Extra assigments, not to output wires
+    assign RULE$respond_rule__RDY = result$out.first__RDY && indication.heard__RDY && result$out.deq__RDY;
     assign fifo$in__enq__RDY_or1 = |fifo$in__enq__RDY_or;
     assign indication.heard$v = result$out.first;
     assign indication.heard__ENA = result$out.first__RDY && result$out.deq__RDY;
@@ -42,10 +44,10 @@ for(__inst$Genvar1 = 0; __inst$Genvar1 < 4; __inst$Genvar1 = __inst$Genvar1 + 1)
         index <= 0;
       end // nRST
       else begin
-        if (result$out.first__RDY && indication.heard__RDY && result$out.deq__RDY) begin // RULE$respond_rule__ENA
+        if (RULE$respond_rule__RDY) begin // RULE$respond_rule__ENA
             $display( "[%s:%d] index %d" , "RULE$respond_rule_block_invoke" , 55 , index );
         end; // End of RULE$respond_rule__ENA
-        if (request.say__ENA && fifo$in__enq__RDY_or1) begin // request.say__ENA
+        if (request.say__ENA && request.say__RDY) begin // request.say__ENA
             $display( "request.say %x index %d" , request.say$v , index );
             if (index >= ( 4 - 1 ))
             index <= 0;
