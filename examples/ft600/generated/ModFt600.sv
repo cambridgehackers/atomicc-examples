24a9bc5cfc4e7a65082950618a7f03f58341d92c
John Ankcorn <jca@alum.mit.edu>
2020-08-28 14:13:34 -0700

    updated ft600

--- examples/ft600/generated/ModFt600.sv
+++ examples/ft600/generated/ModFt600.sv
@@ -13,14 +13,12 @@ module ModFt600 (
     reg [2 - 1:0]usb_rxf_delay;
     reg usb_txe_delay;
     wire CLK;
-    wire iov$T;
     wire nRST;
     IobufVec#(.iovecWidth(16)) iov (
         .IO(usb_ad),
         .I(0),
         .O(),
-        .T(iov$T));
-    assign iov$T = usb_oe_n;
+        .T(usb_rxf_delay[ 0 ]));
     assign usb_oe_n = usb_rxf_delay[ 0 ];
     assign usb_rd_n = !( usb_rxf_delay == 0 );
     assign usb_wr_n = usb_txe_delay || usb_fifo_empty || ( !usb_rxf_delay[ 0 ] );
